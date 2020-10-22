0ce034a73b2f73fd7b75a139910ae245eccee799
Jessie Zhang <wswsws127@gmail.com>
2020-10-22 00:26:50 -0400

    Initial commit

---
+++ AzureTestApp1/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using AzureTestApp1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AzureTestApp1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            myTestDB2Entities te = new myTestDB2Entities();
+            var lst= te.Customers.ToList();
+            return View(lst);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ AzureTestApp1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AzureTestApp1.MvcApplication" Language="C#" %>
